869c429f8db431528ebf42129a561e86bc4d81fb
Georgios Fandridis <fandridis@gmail.com>
2020-02-14 14:30:47 +0200

    feat: basic react structure

---
+++ src/components/App/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      Hello from TODO App
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Todo/TodoList.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function TodoList() {
+  return (
+    <div>
+      This is a list of todos
+    </div>
+  );
+}
+
+export default TodoList;
